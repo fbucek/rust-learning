14d16d5ce0bbb7f4ff30bea42421b7b96749917c
Filip Bucek <fbucek@atlas.cz>
2019-05-31 15:56:18 +0200

    added hashmap and strings

---
+++ hashmap/src/main.rs
@@ -0,0 +1,74 @@
+use std::collections::HashMap;
+
+fn hash_from_vectors() {
+    let teams = vec![String::from("Blue"), String::from("Green")];
+    let initial_scores = vec![10,50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+}
+
+fn invalid_maps() {
+    use std::collections::HashMap;
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    // field_name was moved into map
+    // println!("{}", field_name); <-- error
+
+}
+
+fn or_insert() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+}
+
+fn update_value() {
+    let text = "hello world wonderfull world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
+
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for pair in &scores {
+        println!("{:?}", pair);
+    }
+
+    let vec = vec![1,2];
+
+    assert_eq!(vec[0], 1);
+    
+    hash_from_vectors();
+
+    invalid_maps();
+
+    or_insert();
+
+    update_value();
+}
---
+++ strings/src/main.rs
@@ -0,0 +1,48 @@
+fn multi_language() {
+    let mut vec: Vec<String> = Vec::new();
+
+    vec.push(String::from("السلام عليكم"));
+    vec.push(String::from("Dobrý den"));
+    vec.push(String::from("Hello"));
+    vec.push(String::from("שָׁלוֹם"));
+    vec.push(String::from("नमस्ते"));
+    vec.push(String::from("こんにちは"));
+    vec.push(String::from("안녕하세요"));
+    vec.push(String::from("你好"));
+    vec.push(String::from("Olá"));
+    vec.push(String::from("Здравствуйте"));
+    vec.push(String::from("Hola"));
+
+
+    for str in &vec {
+        println!("{}", str);
+    }
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    println!("{}", s);
+
+    // String lenght
+    let hindi = "नमस्ते";
+    println!("String lenght");
+    println!("{} byte len is: {} bytes", hindi, hindi.len());
+    println!("{} char len is: {:?} bytes", hindi, hindi.chars());
+
+}
+
+
+fn main() {
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+
+    let s = "initial contents".to_string();
+    println!("Hello, world!");
+
+    multi_language();
+}
