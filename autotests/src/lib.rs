5254781febcd0fd4421784fc2f130c0cce08a261
Filip Bucek <fbucek@atlas.cz>
2019-06-02 09:19:26 +0200

    autotests

---
+++ autotests/src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Rectangle;
+
+    #[test]
+    fn can_hold_test() {
+        let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+    
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+}
