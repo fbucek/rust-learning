de43413be31ea6c1aeeca21f713ad530f7c5286e
Filip Bucek <fbucek@atlas.cz>
2019-06-02 12:34:01 +0200

    minigrep: working test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,13 +22,22 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("Text {}", contents.trim());
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -44,7 +53,7 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(
-            vec!["safe, fast, productive"],
+            vec!["safe, fast, productive."],
             search(query, contents)
         );
     }
