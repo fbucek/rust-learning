5fe7d778542ad0833f45fb7ce2ec96360abebc37
Filip Bucek <fbucek@atlas.cz>
2019-06-04 00:04:53 +0200

    reference-cycle

--- reference-cycle/src/main.rs
+++ reference-cycle/src/main.rs
@@ -1,22 +1,27 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use std::cell::RefCell;
-use crate::List::{Cons, Nil};
 
 #[derive(Debug)]
-enum List {
-    Cons(i32, RefCell<Rc<List>>), 
-    Nil,
-}
-
-impl List {
-    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-        match self {
-            Cons(_, item) => Some(item),
-            Nil => None,
-        }
-    }
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("Leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
