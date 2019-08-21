c688bbc0dc5cc3d7c5a0f91718e0ebe3a35bb2f1
Filip Bucek <fbucek@invloop.cz>
2019-08-22 00:25:31 +0200

    update thread

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -3,15 +3,22 @@ use std::time::Duration;
 //use threadlib;
 
 fn main() {
-    let handle = thread::spawn(|| {
+
+    let v = vec![1,2,3];
+
+
+    let handle = thread::spawn( move || {
         for i in 1..10 {
+            println!("Vector v {:?}", v);
             println!("Number {} from thread", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    //drop(v);
+
     handle.join().unwrap();
-    
+
     for i in 1..5 {
         println!("Nubmer {}, from main thread", i);
         thread::sleep(Duration::from_millis(1));
---
+++ threads/src/thread-msg.rs
@@ -0,0 +1,44 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+//use threadlib;
+
+fn main() {
+    let (sender, receiver) = mpsc::channel();
+
+    let sender1 = sender.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("how"),
+            String::from("are"),
+            String::from("you"),
+            String::from("?"),
+        ];
+
+        for val in vals {
+            sender1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+    let vals = vec![
+        String::from("more"),
+        String::from("messages"),
+        String::from("for"),
+        String::from("you"),
+    ];
+
+    for val in vals {
+        sender.send(val).unwrap();
+        thread::sleep(Duration::from_secs(1));
+    }
+});
+
+    for received in receiver {
+        println!("Got: {}", received);
+    }
+}
