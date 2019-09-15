39d5f4423300be51c2fee5de6732e9424422986a
Filip Bucek <fbucek@invloop.cz>
2019-09-15 11:29:03 +0200

    await-async from Jon Gjengset

---
+++ jon-gjengset/await-async/src/main.rs
@@ -0,0 +1,21 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main () {
+    let x = TcpStream::connect("127.0.0.1").unwrap();
+    let y = TcpStream::connect("127.0.0.1").unwrap();
+    x.write("info");
+    y.write("info");
+
+    assert_eq!(x.read().unwrap(), "response");
+    assert_eq!(y.read().unwrap(), "repsonse");
+
+    let x = TcpStream::connect("127.0.0.1")
+        .and_then(|c| c.write("info"))
+        .and_then(|c| c.read())
+        .and_then(|(c, b)|  b == "response");
+    println!("{:?}", fut);
+        
+
+
+}
