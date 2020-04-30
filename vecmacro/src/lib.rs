6c07d771ccdf2121c76906c4f6f0f8fdfb9e86af
Filip Bucek <fbucek@invloop.cz>
2020-04-30 13:37:35 +0200

    added double

--- vecmacro/src/lib.rs
+++ vecmacro/src/lib.rs
@@ -6,6 +6,12 @@ macro_rules! avec {
         vs.push($element);
         vs
     }};
+    ($e1:expr, $e2:expr) => {{
+        let mut vs = Vec::new();
+        vs.push($e1);
+        vs.push($e2);
+        vs
+    }};
 }
 
 #[test]
@@ -24,4 +30,14 @@ fn single() {
     let x: Vec<i32> = avec!(42);
     assert_eq!(x.len(), 1);
     assert_eq!(x[0], 42);
+}
+
+#[test]
+fn double() {
+    let x: Vec<i32> = avec![42, 12];
+    assert_eq!(x.len(), 2);
+    assert_eq!(x[0], 42);
+    assert_eq!(x[1], 12);
+    let x: Vec<i32> = avec!(42, 12);
+    assert_eq!(x.len(), 2);
 }
