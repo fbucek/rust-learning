4b69f754ffc586df1278dc5ca416a7a4785fb108
Filip Bucek <>
2020-08-09 20:37:45 +0200

    shared struct https://youtu.be/b4mS5UPHh20?t=2791

--- gj-panama/src/lib.rs
+++ gj-panama/src/lib.rs
@@ -7,8 +7,22 @@ pub struct Sender<T> {
 
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders += 1;
         Sender {
             shared: Arc::clone(&self.shared),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        let was_last= inner.senders == 0;
+        drop(inner);
+        if was_last {
+            self.shared.available.notify_one();
         }
     }
 }
@@ -16,9 +30,9 @@ impl<T> Clone for Sender<T> {
 impl<T> Sender<T> {
     pub fn send(&mut self, t: T) {
         { 
-            let mut queue = self.shared.queue.lock().unwrap();
-            queue.push_back(t);
-            // drop(queue);
+            let mut inner = self.shared.inner.lock().unwrap();
+            inner.queue.push_back(t);
+            // drop(inner);
         }
         self.shared.available.notify_one();
     }
@@ -29,40 +43,46 @@ pub struct Receiver<T> {
 }
 
 impl<T> Receiver<T> {
-    pub fn recv(&mut self) -> T {
+    pub fn recv(&mut self) -> Option<T> {
         loop {
-            let mut queue = self.shared.queue.lock().unwrap();
-            match queue.pop_front() {
-                Some(t) => return t,
-                None => { 
-                    self.shared.available.wait(queue).unwrap();
+            let mut inner = self.shared.inner.lock().unwrap();
+            match inner.queue.pop_front() {
+                Some(t) => return Some(t),
+                None if inner.senders == 0  => return None,
+                None => {
+                    inner = self.shared.available.wait(inner).unwrap();
                 }
             }
         }
     }
-    pub fn try_recv(&mut self) -> Option<T> {
-        let mut queue = self.shared.queue.lock().unwrap();
-        queue.pop_front()
-    }
+}
+
+struct Inner<T> {
+    queue: VecDeque<T>,
+    senders: usize,
 }
 
 struct Shared<T> {
-    queue: Mutex<VecDeque<T>>,
+    inner: Mutex<Inner<T>>,
     available: Condvar,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let inner = Shared {  
-        queue: Mutex::default(),
+    let inner = Inner { 
+        queue: VecDeque::default(),
+        senders: 1,
+    };
+    let shared = Shared {  
+        inner: Mutex::new(inner),
         available: Condvar::default(),
     };
-    let inner = Arc::new(inner);
+    let shared = Arc::new(shared);
     (
         Sender {
-            shared: inner.clone(),
+            shared: shared.clone(),
         },
         Receiver {
-            shared: inner.clone(),
+            shared: shared.clone(),
         }
     )
 }
@@ -77,14 +97,14 @@ mod tests {
     fn ping_pong_test() {
         let (mut sender, mut receiver) = channel();
         sender.send(41);
-        assert_eq!(receiver.recv(), 41);
+        assert_eq!(receiver.recv(), Some(41));
     }
 
     #[test]
     fn closed() {
-        let (mut sender, mut receiver) = channel();
-        sender.send(41);
-        assert_eq!(receiver.recv(), 41);
+        let (sender, mut receiver) = channel::<()>();
+        let _ = sender;
+        assert_eq!(receiver.recv(), None);
     }
 
 }
